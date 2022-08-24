db185fca0ea3ad5b931ed6033c39ce691c4a7198
Rômulo Freitas <110496787+romulorfreitas@users.noreply.github.com>
2022-08-24 14:55:10 -0400

    Solucao dos requisitos 01 e 02

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,24 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-}
+function compareTrue(value1, value2) {
+  // I declared the 'value1' and 'value2' as a compareTrue object
+  // parameters to reserve a future condition.
+  if (value1 && value2 === true) {
+    return true;
+   } else {
+    return false;
+    }
+  }
+  //Here i used a condition if/else to compare 'value1' and
+  //'value2' as a true bollean value. So if its true then 
+  //return true and if not return false.
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  //Here I set base and height as object calcArea parameters.
+  let areaTotal = (base * altura)/2;
+  //Here I created a new variable called 'areaTotal' to obtain 
+  // the triangle area.
+  return areaTotal;
 }
 
 // Desafio 3
