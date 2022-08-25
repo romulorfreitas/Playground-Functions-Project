e78bcc68eeeef1a098333d9157b43a5977eb9f6d
Rômulo Freitas <110496787+romulorfreitas@users.noreply.github.com>
2022-08-25 12:59:07 -0400

    Entrega dos requisitos 03 e 04

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,7 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  // I declared the 'value1' and 'value2' as a compareTrue object
-  // parameters to reserve a future condition.
-  if (value1 && value2 === true) {
+  // I declared the 'value1' and 'value2' as a compareTrue parameter
+   if (value1 && value2 === true) {
     return true;
    } else {
     return false;
@@ -14,22 +13,40 @@ function compareTrue(value1, value2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  //Here I set base and height as object calcArea parameters.
-  let areaTotal = (base * altura)/2;
-  //Here I created a new variable called 'areaTotal' to obtain 
-  // the triangle area.
-  return areaTotal;
-}
+  //Here I set base and height as calcArea parameter.
+  let totalArea = ((base * height)/2);
+  //Here I created a new variable called 'totalArea' to have base  
+  // times height divided by two to obtain the triangle area.
+  return totalArea;
+ //console.log(totalArea);
+ }
+ //calcArea(10,50)
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(phrase){
+// Here I addedd a parameter called 'phrase' to a splitSencente function
+//console.log(string.split (' '));
+return phrase.split (' ');
 }
+// Here I added a return using the method split to have a return with
+// new array separated with comma and space between each word from
+// parameter phrase. 
+//splitSentence('go trybe')
+// Here I added a value to phrase parameter.
+  
+
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
-}
+function concatName(emptyArray) {
+// I added a value called emptyArray to function concatName parameter.  
+  let fisrtLast = emptyArray[emptyArray.length -1] + ', ' + emptyArray[0];
+  // Next i created a variable called fisrtLast to have the last 
+  // emptyArray item plus one string with comma and space plus the
+  // first item from emptyArray. 
+  // console.log(fisrtLast);
+  return fisrtLast;
+}  
+// concatName(['Frodo', 'Gandalf', 'Boromir', 'Legolas','Sam'])
 
 // Desafio 5
 function footballPoints() {
